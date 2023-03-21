e213c50dfe2d7e204565cff7678bd5d0409f21e6
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-03-21 21:55:44 +0000

    made calculations work

--- index.php
+++ index.php
@@ -1,42 +1,40 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en-ca">
   <head>
-    
-    <!-- Metadata -->
+
+    <!--Meta data section-->
     <meta charset="utf-8">
     <meta name="description" content="Salary, with PHP">
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="ZoiaB">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- Code for the favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/android-chrome-512x512.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/android-chrome-512x512.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/android-chrome-512x512.png">
-    <link rel="manifest" href="./fav_index/android-chrome-512x512.png">
-    
-    <!-- Stylesheet -->
+
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="180x180" href="./fav_index/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./fav_index/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./fav_index/favicon-16x16.png">
+    <link rel="manifest" href="./fav_index/site.webmanifest">
     <link rel="stylesheet" href="./css/style.css">
-    
+
     <!-- Title -->
     <title>Salary, with PHP</title>
   </head>
   <body>
-    <?php 
+ <?php 
       echo "<h1>Salary, with PHP</h1>";
-      echo "<h3>Enter your information here:</h3>";
+      echo "<h3>Enter your information:</h3>";
     ?>
 
     <!-- Form for user input -->
     <form action="./results.php" method="post" target="result">
       <label for="hours">Number of hours</label>
-      <input type="float" id="number of hours" placeholder="Number of hours..." name="number of hours"><br><br>
+      <input type="float" id="number-of-hours" placeholder="Number of hours..." name="number-of-hours"><br><br>
       <label for="rate">Hourly rate $</label>
-      <input type="float" id="hourly rate" placeholder="Hourly rate..." name="hourly-rate"><br><br>
+      <input type="float" id="hourly-rate" placeholder="Hourly rate..." name="hourly-rate"><br><br>
       <input type="submit" value="Calculate">
     </form>
-    
-    <!-- Calculations -->
+
+    <!-- Div for calculations -->
     <iframe id="result" name="result">
     </iframe>
   </body>
--- results.php
+++ results.php
@@ -1,14 +1,13 @@
 <?php 
-  //Declaring constant tax
+  //Declaring a constant for tax rate
   define("TAX_RATE", 0.1805);
-  //Declaring other variables and converting to floats
-  $hours = floatval($_POST["number of hours"]);
-  $rate = floatval($_POST["hourly rate"]);
+  //Declaring other variables and converting the strings from the form to floats
+  $hours = floatval($_POST["number-of-hours"]);
+  $rate = floatval($_POST["hourly-rate"]);
   $total = $hours * $rate;
   //number_format($number, $numberOfDecimals) rounds off decimals
   $tax = number_format($total * TAX_RATE, 2);
   $pay = number_format($total - $tax, 2);
 ?>
 
-You will get $<?php echo "$pay" ?>. <br>
-The government will take $<?php echo "$tax" ?>.
+You will get $<?php echo "$pay" ?>. <br>The government will take $<?php echo "$tax" ?>.
